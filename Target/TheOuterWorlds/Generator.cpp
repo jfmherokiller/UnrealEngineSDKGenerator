160294fea2cf3d35589f3702d341a4f4d835399b
noah the goodra <peterpan0413@live.com>
2021-09-14 22:13:07 -0500

    AddFTextData

--- Target/TheOuterWorlds/Generator.cpp
+++ Target/TheOuterWorlds/Generator.cpp
@@ -588,9 +588,23 @@ public:
 	}
 };
 
-struct FText
-{
-	char UnknownData[0x18];
+class FTextData {
+public:
+	char UnknownData[0x28];
+	wchar_t* Name;
+	__int32 Length;
+};
+
+struct FText {
+	FTextData* Data;
+	char UnknownData[0x10];
+
+	wchar_t* Get() const {
+		if (Data)
+			return Data->Name;
+
+		return nullptr;
+	}
 };
 
 struct FScriptDelegate
